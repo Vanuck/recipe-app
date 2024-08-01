0aaf3bfaf18a3de9da642daa7f69d5303ce1cf9a
Vanuck <andrew.jornitz@zoho.com>
2024-08-01 11:56:55 -0700

    Update settings.py

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0v3bn=i%-arlz9x=zj6p)extwao-p6=^%d43%6+)1i_3t9+@sz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
